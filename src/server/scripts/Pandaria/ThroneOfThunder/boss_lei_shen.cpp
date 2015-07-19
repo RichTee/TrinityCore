884790c210b160a5d4148c3f697ec1517edca362
MrRichTee <ostmyst@gmail.com>
2015-07-19 13:21:59 +0200

    Core/Boss: remove warning

--- src/server/scripts/Pandaria/ThroneOfThunder/boss_lei_shen.cpp
+++ src/server/scripts/Pandaria/ThroneOfThunder/boss_lei_shen.cpp
@@ -171,7 +171,7 @@ public:
         }
 
 
-        void UpdateAI(uint32 const diff) override
+        void UpdateAI(uint32 diff) override
         {
             if (!UpdateVictim())
                 return;
